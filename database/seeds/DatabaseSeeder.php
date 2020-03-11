ce0ab3bd3dd8f99749831ea51de2f578ee3190bc
phamthai12316 <phamthai12316@gmail.com>
2020-03-11 11:45:18 +0700

    thai-amado-t1904a

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 //         $this->call(CategorySeeder::class);
 //         $this->call(BrandSeeder::class);
-//         $this->call(ProductSeeder::class);
+         $this->call(ProductSeeder::class);
 
     }
 }
