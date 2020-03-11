5a60f749a446ca5d5f1b683031c0389ef5cb5e66
phamthai12316 <phamthai12316@gmail.com>
2020-03-11 17:55:05 +0700

    add migration

--- config/database.php
+++ config/database.php
@@ -15,7 +15,8 @@ return [
     |
     */
 
-   'default' => env('DB_CONNECTION', 'mysql'),
+//   'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'pgsql',
 
 
     /*
@@ -66,12 +67,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' =>env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' =>env('DB_PASSWORD', ''),
+//            'url' => env('DATABASE_URL'),
+//            'host' =>env('DB_HOST', '127.0.0.1'),
+//            'port' => env('DB_PORT', '5432'),
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' =>env('DB_PASSWORD', ''),
+            'host' =>'ec2-34-200-101-236.compute-1.amazonaws.com',
+            'port' => '5432',
+            'database' => 'dbug4nv24jcvul',
+            'username' => 'ynrebiebngytha',
+            'password' =>'fade56a60a79d828a1caaa4745f9a53b8de1f3582b6eb69a146f86b0fed448e4',
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
